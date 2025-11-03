26866da12250ce293aea5178997fc0284a073190
stupidman111 <2456306700@qq.com>
2025-11-03 16:44:06 +0800

    09: projects

---
+++ src/modules/projects/server/procedures.ts
@@ -0,0 +1,56 @@
+import { inngest } from "@/inngest/client";
+import { prisma } from "@/lib/db";
+import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import { generateSlug } from "random-word-slugs";
+import { z } from "zod";
+
+export const projectsRouter = createTRPCRouter({
+  getMany: baseProcedure.query(async () => {
+    const projects = await prisma.project.findMany({
+      orderBy: {
+        updateAt: "asc",
+      },
+      // include: {
+      //   fragment: true,
+      // },
+    });
+
+    return projects;
+  }),
+
+  create: baseProcedure
+    .input(
+      z.object({
+        value: z
+          .string()
+          .min(1, { message: "Value is required" })
+          .max(10000, { message: "Value is too long" }),
+      })
+    )
+    .mutation(async ({ input }) => {
+      const createdProject = await prisma.project.create({
+        data: {
+          name: generateSlug(2, {
+            format: "kebab",
+          }),
+          message: {
+            create: {
+              content: input.value,
+              role: "USER",
+              type: "RESULT",
+            },
+          },
+        },
+      });
+
+      await inngest.send({
+        name: "code-agent/run",
+        data: {
+          value: input.value,
+          projectId: createdProject.id,
+        },
+      });
+
+      return createdProject;
+    }),
+});
