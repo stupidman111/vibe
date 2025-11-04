d29c2de9647918d2a7d54c5e86f98e5496f5426f
stupidman111 <2456306700@qq.com>
2025-11-04 17:35:42 +0800

    10: messages ui

--- src/modules/projects/server/procedures.ts
+++ src/modules/projects/server/procedures.ts
@@ -1,10 +1,33 @@
 import { inngest } from "@/inngest/client";
 import { prisma } from "@/lib/db";
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import { TRPCError } from "@trpc/server";
 import { generateSlug } from "random-word-slugs";
 import { z } from "zod";
 
 export const projectsRouter = createTRPCRouter({
+  getOne: baseProcedure
+    .input(
+      z.object({
+        id: z.string().min(1, { message: "ID is required" }),
+      })
+    )
+    .query(async ({ input }) => {
+      const existingProject = await prisma.project.findUnique({
+        where: {
+          id: input.id,
+        },
+      });
+
+      if (!existingProject) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Project not found",
+        });
+      }
+      return existingProject;
+    }),
+
   getMany: baseProcedure.query(async () => {
     const projects = await prisma.project.findMany({
       orderBy: {
---
+++ src/modules/projects/ui/components/message-card.tsx
@@ -0,0 +1,174 @@
+import { Card } from "@/components/ui/card";
+import { Fragment, MessageRole, MessageType } from "@/generated/prisma";
+import { cn } from "@/lib/utils";
+import { format } from "date-fns";
+import { ChevronRightIcon, Code2Icon } from "lucide-react";
+import Image from "next/image";
+
+/**
+ * 用户消息 props
+ */
+interface UserMessageProps {
+  content: string;
+}
+
+/**
+ * 用户消息
+ * @param param0
+ * @returns
+ */
+const UserMessage = ({ content }: UserMessageProps) => {
+  return (
+    <div className="flex justify-end pb-4 pr-2 pl-10">
+      <Card className="rounded-lg bg-muted p-3 shadow-none border-none max-w-[80%] break-words">
+        {content}
+      </Card>
+    </div>
+  );
+};
+
+/**
+ * 代码片段 props
+ */
+interface FragmentCardProps {
+  fragment: Fragment;
+  isActiveFragment: boolean;
+  onFragmentClick: (fragment: Fragment) => void;
+}
+
+/**
+ * 代码片段
+ * @param param0
+ * @returns
+ */
+const FragmentCard = ({
+  fragment,
+  isActiveFragment,
+  onFragmentClick,
+}: FragmentCardProps) => {
+  return (
+    <button
+      className={cn(
+        "flex items-start text-start gap-2 border rounded-lg bg-muted w-fit p-3 hover:bg-secondary transition-colors",
+        isActiveFragment &&
+          "bg-primary text-primary-foreground border-primary hover:bg-primary"
+      )}
+      onClick={() => onFragmentClick(fragment)}
+    >
+      <Code2Icon className="size-4 mt-0.5" />
+      <div className="flex flex-col flex-1">
+        <span className="text-sm font-medium line-clamp-1">
+          {fragment.title}
+        </span>
+        <span className="text-sm">Preview</span>
+      </div>
+      <div className="flex items-center justify-center mt-0.5">
+        <ChevronRightIcon className="size-4" />
+      </div>
+    </button>
+  );
+};
+
+/**
+ * 助手消息 props
+ */
+interface AssistantMessageProps {
+  content: string;
+  fragment: Fragment | null;
+  createdAt: Date;
+  isActiveFragment: boolean;
+  onFragmentClick: (fragment: Fragment) => void;
+  type: MessageType;
+}
+
+/**
+ * 助手消息
+ * @param param0
+ * @returns
+ */
+const AssistantMessage = ({
+  content,
+  fragment,
+  createdAt,
+  isActiveFragment,
+  onFragmentClick,
+  type,
+}: AssistantMessageProps) => {
+  return (
+    <div
+      className={cn(
+        "flex flex-col group px-2 pb-4",
+        type === "ERROR" && "text-red-700 dark:text-red-500"
+      )}
+    >
+      <div className="flex items-center gap-2 pl-2 mb-2">
+        <Image
+          src="/logo.svg"
+          alt="Vibe"
+          width={18}
+          height={18}
+          className="shrink-0"
+        />
+        <span className="text-sm font-medium">Vibe</span>
+        <span
+          className="text-xs text-muted-foreground opacity-0 
+                    transition-opacity group-hover:opacity-100"
+        >
+          {format(createdAt, "HH:mm 'on' MMM dd, yyyy")}
+        </span>
+      </div>
+      <div className="pl-8.5 flex flex-col gap-y-4">
+        <span>{content}</span>
+        {fragment && type === "RESULT" && (
+          <FragmentCard
+            fragment={fragment}
+            isActiveFragment={isActiveFragment}
+            onFragmentClick={onFragmentClick}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+/**
+ * 消息卡片 props
+ */
+interface MessageCardProps {
+  content: string;
+  role: MessageRole;
+  fragment: Fragment | null;
+  createdAt: Date;
+  isActiveFragment: boolean;
+  onFragmentClick: (fragment: Fragment) => void;
+  type: MessageType;
+}
+
+/**
+ * 消息卡片
+ * @param param0
+ * @returns
+ */
+export const MessageCard = ({
+  content,
+  role,
+  fragment,
+  createdAt,
+  isActiveFragment,
+  onFragmentClick,
+  type,
+}: MessageCardProps) => {
+  if (role === MessageRole.ASSISTANT) {
+    return (
+      <AssistantMessage
+        content={content}
+        fragment={fragment}
+        createdAt={createdAt}
+        isActiveFragment={isActiveFragment}
+        onFragmentClick={onFragmentClick}
+        type={type}
+      />
+    );
+  }
+  return <UserMessage content={content} />;
+};
