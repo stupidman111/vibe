9c2b4c0c4012d521ad75b794cee38bf7215f8c5d
stupidman111 <2456306700@qq.com>
2025-11-12 20:43:53 +0800

    20: deployment

--- prisma.config.ts
+++ prisma.config.ts
@@ -11,15 +11,15 @@
 //   },
 // });
 
-import "dotenv/config";
-import { defineConfig } from "prisma/config";
-export default defineConfig({
-  schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
-  engine: "classic",
-  datasource: {
-    url: process.env.DATABASE_URL!,
-  },
-});
+// import "dotenv/config";
+// import { defineConfig } from "prisma/config";
+// export default defineConfig({
+//   schema: "prisma/schema.prisma",
+//   migrations: {
+//     path: 'prisma/migrations'
+//   },
+//   engine: "classic",
+//   datasource: {
+//     url: process.env.DATABASE_URL!,
+//   },
+// });
